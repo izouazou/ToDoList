7c484ca6bf8889f1140e1248c42a8a746f773402
zvvazo <zvvazo@gmail.com>
2023-12-04 02:01:13 +0100

    Adding React App to Django Project

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("api/", include("base.urls")),
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
