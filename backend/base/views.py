449dc8a4005dccdc63b36bb55dc9fbc8fad604c1
zvvazo <zvvazo@gmail.com>
2023-12-04 01:11:38 +0100

    Adding Django REST Framework

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
 
 
-
+@api_view(['GET'])
 def index(request):
-    return HttpResponse("Hello, world. You're at the ToDo List index.")
+    return Response("Hello, world. You're at the ToDo List index.")
